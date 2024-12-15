d5205499fb23899d9e4bb52af07401e35917ede0
SCPassion <141584825+SCPassion@users.noreply.github.com>
2024-12-15 17:44:07 +0100

    Added fetch APi & update corresponding UI

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const form = document.getElementById('form');
-const color = document.getElementById('color');
-const scheme = document.getElementById('scheme');
+const form = document.getElementById('form')
+const color = document.getElementById('color')
+const scheme = document.getElementById('scheme')
+const colorHex = [...document.getElementsByClassName('color-hex')]
+
+const baseURL = "https://www.thecolorapi.com"
+const endpoint = "/scheme"
 
 color.value = "#F55A5A"
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form)
-    const data = {hex: formData.get('color'), scheme:formData.get('scheme') }
+    // const data = {hex: formData.get('color'), scheme:formData.get('scheme') }
+    const url = `${baseURL}${endpoint}?hex=${formData.get('color').slice(1)}&count=5&mode=${formData.get('scheme')}`
+    fetch(url)
+        .then(res=>res.json())
+        .then(data=> {
+            data.colors.forEach((color, index)=> {
+                colorHex[index].textContent = color.hex.value
+                document.getElementById(colorHex[index].dataset.id).style.backgroundColor = color.hex.value
+            })
+            // console.log(data.colors)
+        })
 });
 
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&count=5&mode=analogic")
-    .then(res=>res.json())
-    .then(data=>console.log(data.colors))
+// fetch("https://www.thecolorapi.com/scheme?hex=0047AB&count=5&mode=analogic")
+//     .then(res=>res.json())
+//     .then(data=>console.log(data.colors))
